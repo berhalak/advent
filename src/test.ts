ac169ff96022a6013a1823de74cd347c234e5fbb
berhalak <jaroslaw.sadzinski@gmail.com>
2019-12-04 06:41:17 +0100

    Prepering for 05

--- src/test.ts
+++ src/test.ts
@@ -1,49 +1,5 @@
 import { lines } from "./lib";
 
-console.log(lines().invert());
+let final = 0;
 
-let start = 138307;
-let end = 654504;
-let count = 0;
-
-
-function test(g: number) {
-    let f = g.toString();
-    let p = f.split('');
-
-    // test same number
-    let grouped = p.group(x => x);
-    if (!grouped.some(x => x.list.length >= 2))
-        return false;
-
-    if (f != p.orderBy(x => x).join('')) {
-        return false;
-    }
-
-    return true;
-}
-
-function test2(g: number) {
-    let f = g.toString();
-
-    let p = f.split('');
-
-    // test same number
-    let grouped = p.group(x => x);
-    if (!grouped.some(x => x.list.length == 2))
-        return false;
-
-
-    if (f != p.orderBy(x => x).join('')) {
-        return false;
-    }
-
-
-    return true;
-}
-
-for (let i = start; i <= end; i++) {
-    if (test2(i)) count++;
-}
-
-console.log(count);
+console.log(final);

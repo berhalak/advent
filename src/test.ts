f01e6410c373cdf688021f4f691a2343b5f79983
berhalak <jaroslaw.sadzinski@gmail.com>
2019-12-04 06:29:54 +0100

    Advent4

--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,73 @@
 import { lines } from "./lib";
 
 console.log(lines().invert());
+
+let start = 138307;
+let end = 654504;
+let count = 0;
+
+
+function test(g: number) {
+    let f = g.toString();
+
+    let p = f.split('');
+
+    // test same number
+    let ok = false;
+    for (let i = 0; i < p.length - 1; i++) {
+        if (p[i] == p[i + 1]) {
+            ok = true;
+            break;
+        }
+    }
+
+    if (!ok) return false;
+
+    ok = true;
+
+    for (let i = 0; i < p.length - 1; i++) {
+        if (p[i] > p[i + 1]) {
+            ok = false;
+            break;
+        }
+    }
+
+    return ok;
+}
+
+function test2(g: number) {
+    let f = g.toString();
+
+    let p = f.split('');
+
+    // test same number
+    let sums: any = {};
+    for (let i = 0; i < p.length - 1; i++) {
+        if (p[i] == p[i + 1]) {
+            sums[p[i]] = sums[p[i]] || 0;
+            sums[p[i]]++;
+        }
+    }
+
+    if (!Object.values(sums).includes(1)) {
+        return false;
+    }
+
+
+    let ok = true;
+
+    for (let i = 0; i < p.length - 1; i++) {
+        if (p[i] > p[i + 1]) {
+            ok = false;
+            break;
+        }
+    }
+
+    return ok;
+}
+
+for (let i = start; i <= end; i++) {
+    if (test2(i)) count++;
+}
+
+console.log(count);

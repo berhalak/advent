74d41b7cc30128c0979b646b3a9e8581925f005f
berhalak <jaroslaw.sadzinski@gmail.com>
2019-12-10 00:13:12 +0100

    07 part 1

---
+++ src/prog.ts
@@ -0,0 +1,233 @@
+class Add {
+    constructor(private pos: string) {
+
+    }
+    invoke(index: number, input: number[], memory: number[]) {
+        let a = memory[index + 1];
+        let b = memory[index + 2];
+        let c = memory[index + 3];
+        let amode = this.pos[2];
+        let bmode = this.pos[1];
+
+        if (amode == '0') {
+            a = memory[a];
+        }
+        if (bmode == '0') {
+            let t = memory[b];
+            if (t == undefined) {
+                debugger;
+            }
+            b = t;
+        }
+
+        memory[c] = a + b;
+        return index + 3;
+    }
+}
+
+class Mul {
+    constructor(private pos: string) {
+
+    }
+
+    invoke(index: number, input: number[], memory: number[]) {
+        let a = memory[index + 1];
+        let b = memory[index + 2];
+        let c = memory[index + 3];
+        let amode = this.pos[2];
+        let bmode = this.pos[1];
+
+        if (amode == '0') {
+            a = memory[a];
+        }
+        if (bmode == '0') {
+            b = memory[b];
+        }
+        memory[c] = a * b;
+        return index + 3;
+    }
+}
+
+class Store {
+    constructor(private pos: string) {
+
+    }
+
+    invoke(index: number, input: number[], memory: number[]) {
+        let a = memory[index + 1];
+        memory[a] = input.shift() as number;
+        return index + 1;
+    }
+}
+
+class Out {
+    constructor(private pos: string) {
+
+    }
+    invoke(index: number, input: number[], memory: number[]) {
+        let a = memory[index + 1];
+        let amode = this.pos[2];
+
+        if (amode == '0') {
+            a = memory[a];
+        }
+        (input as any).out = a;
+        //console.log(a);
+        return index + 1;
+    }
+}
+
+class JumpFalse {
+    constructor(private pos: string) {
+
+    }
+
+    invoke(index: number, input: number[], memory: number[]) {
+        let a = memory[index + 1];
+        let b = memory[index + 2];
+        let c = memory[index + 3];
+        let amode = this.pos[2];
+        let bmode = this.pos[1];
+
+        if (amode == '0') {
+            a = memory[a];
+        }
+        if (bmode == '0') {
+            b = memory[b];
+        }
+        if (a == 0) {
+            return b - 1;
+        }
+        return index + 2;
+    }
+}
+
+
+class Equal {
+    constructor(private pos: string) {
+
+    }
+
+    invoke(index: number, input: number[], memory: number[]) {
+        let a = memory[index + 1];
+        let b = memory[index + 2];
+        let c = memory[index + 3];
+        let amode = this.pos[2];
+        let bmode = this.pos[1];
+
+        if (amode == '0') {
+            a = memory[a];
+        }
+        if (bmode == '0') {
+            b = memory[b];
+        }
+        memory[c] = a == b ? 1 : 0;
+        return index + 3;
+    }
+}
+
+class LessThen {
+    constructor(private pos: string) {
+
+    }
+
+    invoke(index: number, input: number[], memory: number[]) {
+        let a = memory[index + 1];
+        let b = memory[index + 2];
+        let c = memory[index + 3];
+        let amode = this.pos[2];
+        let bmode = this.pos[1];
+
+        if (amode == '0') {
+            a = memory[a];
+        }
+        if (bmode == '0') {
+            b = memory[b];
+        }
+        memory[c] = a < b ? 1 : 0;
+        return index + 3;
+    }
+}
+
+class JumpTrue {
+    constructor(private pos: string) {
+
+    }
+
+    invoke(index: number, input: number[], memory: number[]) {
+        let a = memory[index + 1];
+        let b = memory[index + 2];
+        let c = memory[index + 3];
+        let amode = this.pos[2];
+        let bmode = this.pos[1];
+
+        if (amode == '0') {
+            a = memory[a];
+        }
+        if (bmode == '0') {
+            b = memory[b];
+        }
+        if (a != 0) {
+            return b - 1;
+        }
+        return index + 2;
+    }
+}
+
+function instruction(op: number) {
+    let str = op.toString().padStart(5, '0');
+    let code = str.split('').skip(3).take(2).join('').toNumber();
+    let pos = str.split('').take(3).join('');
+    if (code == 1) {
+        return new Add(pos);
+    }
+    if (code == 2) {
+        return new Mul(pos);
+    }
+    if (code == 3) {
+        return new Store(pos);
+    }
+    if (code == 4) {
+        return new Out(pos);
+    }
+    if (code == 5) {
+        return new JumpTrue(pos);
+    }
+    if (code == 6) {
+        return new JumpFalse(pos);
+    }
+    if (code == 7) {
+        return new LessThen(pos);
+    }
+    if (code == 8) {
+        return new Equal(pos);
+    }
+    if (code == 99) {
+        return null;
+    }
+}
+
+export function run(input: number[], code: number[]): number {
+
+    let myInput = [...input];
+    let memory = [...code];
+
+    for (let i = 0; i < memory.length; i++) {
+        let opcode = memory[i];
+
+        let ins = instruction(opcode);
+        if (ins == null) {
+            break;
+        }
+        let delta = ins.invoke(i, myInput, memory);
+        i = delta;
+    }
+
+    return (myInput as any).out;
+}
+
+
+
+//let memory = read().split(',').map(x => x.toNumber());
+
+//console.log(memory);

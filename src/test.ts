844cd7b03c1310a557f3023da74b5b3463c41848
berhalak <jaroslaw.sadzinski@gmail.com>
2019-12-17 03:17:45 +0100

    p1

--- src/test.ts
+++ src/test.ts
@@ -1,297 +1,94 @@
 import { lines, read, Point } from "./lib";
 import { Runtime, Env, Canvas, readCode } from "./prog";
-import { getHeapStatistics } from "v8";
 
-const wall = 0;
-const free = 1;
-const oxygen = 2;
-
-const north = 1;
-const south = 2;
-const west = 3;
-const east = 4;
-
-class Plan extends Canvas<string> {
-
-	clone() {
-		let cl = new Plan(' ');
-		cl.data = JSON.parse(JSON.stringify(this.data));
-		return cl;
-	}
-
-	constructor(def: string) {
-		super(def);
-	}
-
-	protected paintCell(c: string) {
-		return c;
-	}
-}
-
-class Direction {
-
-	constructor(private cur = north) {
-
-	}
-
-	dir(): number {
-		return this.cur;
-	}
-
-	toString() {
-		if (this.cur == north) {
-			return '^'
-		}
-		if (this.cur == east) {
-			return '>';
-		}
-		if (this.cur == west) {
-			return '<';
-		}
-		return 'v';
-	}
-
-	next(pos: Point): Point {
-		if (this.cur == north) {
-			return pos.up();
-		}
-		if (this.cur == east) {
-			return pos.right();
-		}
-		if (this.cur == west) {
-			return pos.left();
-		}
-		return pos.down();
-	}
-
-	turnLeft() {
-		let c = this.cur;
-
-		c = c == north ? west :
-			c == east ? north :
-				c == south ? east :
-					south;
-
-		return new Direction(c);
-
-
-	}
-	turnRight() {
-		let next = this.cur;
-
-		next = next == north ? east :
-			next == east ? south :
-				next == south ? west :
-					north;
-
-		return new Direction(next);
-	}
-
-}
-
-let plan = new Plan(' ');
-
-
-function sleep(seconds = 1000) {
-
-	var waitTill = new Date(new Date().getTime() + 5);
-	while (waitTill > new Date()) { }
-}
-
-
-// draw droid on center
-plan.set(0, 0, '.');
-
-
-interface Robot {
-	drawMap(): void;
-	left(): boolean;
-	down(): boolean;
-	right(): boolean;
-	up(): boolean;
-}
-
-const dot_was = '■';
-
-
-class Remote implements Env, Robot {
-	input(): number {
-		return this.toSend;
-	}
-
-	drawMap(): void {
-
-		sleep(500);
-		console.clear();
-
-		let was = plan.at(this.pos);
-		plan.paint(this.pos, 'D');
-		plan.dump();
-		plan.paint(this.pos, was);
-	}
-
-	left(): boolean {
-		this.next = this.pos.left();
-		return this.run(west);
-	}
-	down(): boolean {
-		this.next = this.pos.down();
-		return this.run(south);
-	}
-	right(): boolean {
-		this.next = this.pos.right();
-		return this.run(east);
-	}
-
-	up(): boolean {
-		this.next = this.pos.up();
-		return this.run(north);
-	}
-
-	private run(dir: number) {
-		this.toSend = dir;
-		this.runtime.execute(this);
-
-		if (this.result == free) {
-			this.drawMap();
-		}
-
-		return this.result == free;
-	}
-
-	private toSend = north;
-
-	pos = new Point(0, 0);
-	private next = new Point(0, 0);
-	private runtime = new Runtime(readCode());
-	private result = free;
-	ox: Point | null = null;
-	private count = 0;
-
-	output(a: number): boolean {
-		this.result = a;
-		if (a == free) {
-			let was = plan.at(this.next);
-			if (was == ' ') {
-				this.count++;
-				plan.paint(this.pos, dot_was);
-				plan.paint(this.next, dot_was);
-			} else {
-				plan.paint(this.next, '.');
-				plan.paint(this.pos, '.');
-			}
-			this.pos = this.next;
-		} else if (a == wall) {
-			this.wallAt(this.next);
-		} else if (a == oxygen) {
-			plan.paint(this.next, "O");
-			this.pos = this.next;
-			this.ox = this.pos;
-			this.result = free;
-		}
-		return false;
-	}
-
-	private wallAt(next: Point) {
-		plan.paint(next, '#');
-	}
+let raw = read().split('').map(x => x.toNumber());
+let pattern = [0, 1, 0, -1];
+let input = [...raw];
+for (let i = 0; i < 10000; i++) {
+	input.push(...raw);
 }
 
 
 
-class Compas {
-	cur = north;
-	rob = new Remote();
-	shortest = 0;
-	drive() {
 
-		if (this.rob.ox && !this.shortest) {
-			this.shortest = ([...plan.all()].filter(x => x == dot_was).length);
+function calcNumber(pos: number, nums: number[]): number {
+	let sum = 0;
+	for (let i = pos; i < nums.length;) {
+		for (let t = i; t <= i + pos && t < nums.length; t++) {
+			sum += nums[t];
 		}
-
-		switch (this.cur) {
-			case north: return this.rob.up();
-			case east: return this.rob.right();
-			case south: return this.rob.down();
-			case west: return this.rob.left();
+		let subStart = i + pos + 1 + pos;
+		let subEnd = subStart + pos;
+		for (let t = subStart; t <= subEnd && t < nums.length; t++) {
+			sum -= nums[t];
 		}
-
+		i += 4 * (pos + 1);
 	}
-
-	left() {
-		switch (this.cur) {
-			case north: return this.cur = west; break;
-			case east: return this.cur = north; break;
-			case south: return this.cur = east; break;
-			case west: return this.cur = south; break;
-		}
-	}
-
-	right() {
-		switch (this.cur) {
-			case north: return this.cur = east; break;
-			case east: return this.cur = south; break;
-			case south: return this.cur = west; break;
-			case west: return this.cur = north; break;
-		}
-	}
+	return Math.abs(sum) % 10;
 }
 
-let robot = new Compas();
+console.log(input.length);
 
+function transform(input: number[]): number[] {
+	let out: number[] = [];
 
-while (robot.drive()) { }
-
-do {
-	robot.left();
-	if (robot.drive()) continue;
-	robot.right();
-	if (robot.drive()) continue;
-	robot.right();
-	if (robot.drive()) continue;
-	robot.right();
-	if (robot.drive()) continue;
-
-} while (!robot.rob.pos.equals(new Point(0, 0)));
-
-
-
-let ox = robot.rob.ox as Point;
-plan.paint(ox, 'O');
-let time = 0;
-while (true) {
-	// get all oxygen
-	let start = plan.clone();
-	let spread = false;
-	for (let o of start.iter()) {
-		let isOx = start.at(o) == 'O';
-		if (isOx) {
-			if (start.at(o.up()) == '.') {
-				plan.paint(o.up(), 'O');
-				spread = true;
-			}
-			if (start.at(o.down()) == '.') {
-				plan.paint(o.down(), 'O');
-				spread = true;
-			}
-			if (start.at(o.left()) == '.') {
-				plan.paint(o.left(), 'O');
-				spread = true;
-			}
-			if (start.at(o.right()) == '.') {
-				plan.paint(o.right(), 'O');
-				spread = true;
-			}
-		}
+	for (let i = 1746510; i <= 1746510 + 8; i++) {
+		out.push(calcNumber(i, input));
 	}
-	console.clear();
-	plan.dump();
-	if (!spread) {
-		console.log("Time was " + time);
-		break;
+
+
+	for (let i = 1746510; i <= 1746510 + 8; i++) {
+		input[i] = out[i - 1746510];
 	}
-	sleep();
-	time++;
+
+	return input;
 }
 
-console.log("Shortest was " + robot.shortest);
+let phase = 0;
+while (phase != 100) {
+	phase++;
+	input = transform(input);
+}
+
+let out = input.skip(1746510).take(8).join('').toNumber();
+console.log(out);
+
+
+
+// function calcNumber(pos: number, nums: number[]): number {
+
+
+
+// 	let patt = prepere(pos) as number[];
+
+// 	let calc: number[] = [];
+
+// 	nums.forEach((a, i) => {
+// 		calc[i] = a * patt[i];
+// 	});
+
+// 	let sum = calc.sum();
+
+// 	let res = Math.abs(sum) % 10;
+// 	return res;
+// }
+
+
+// function transform(input: number[]): number[] {
+// 	let out = [];
+
+// 	for (let i = 0; i < input.length; i++) {
+// 		out.push(calcNumber(i, input));
+// 	}
+
+// 	return out;
+// }
+
+// while (phase != 100) {
+// 	phase++;
+// 	input = transform(input);
+
+// 	let eight = input.take(8).join('');
+// 	console.log(`After ${phase} phase: ${eight}`);
+// }
+
